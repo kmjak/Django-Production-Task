bbfc7f31dbd8fe7f508eb1753700c18ccd67b7dc
kmjak <tymkoron1221@icloud.com>
2024-11-15 16:56:33 +0900

    Employeeのログイン用のformを書いた

---
+++ django_app/employee/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Employee
+
+class EmployeeLoginForm(forms.Form):
+    employee_id = forms.CharField(
+        max_length=6,
+        label="従業員ID",
+        widget=forms.TextInput(attrs={
+            'class': 'block border-2 border-black rounded-lg w-full px-3 py-1 opacity-30 hover:opacity-50 focus:opacity-100 transition-all',
+            'id': 'employee-id',
+            })
+    )
+    employee_name = forms.CharField(
+        max_length=200,
+        label="従業員名",
+        widget=forms.TextInput(attrs={
+            'class': 'block border-2 border-black rounded-lg w-full px-3 py-1 opacity-30 hover:opacity-50 focus:opacity-100 transition-all',
+            'id': 'employee-name',
+        })
+    )
+    employee_password = forms.CharField(
+        max_length=200,
+        label="パスワード",
+        widget=forms.PasswordInput(attrs={
+            'class': 'block border-2 border-black rounded-lg w-full px-3 py-1 opacity-30 hover:opacity-50 focus:opacity-100 transition-all',
+            'id': 'password',
+        })
+    )
