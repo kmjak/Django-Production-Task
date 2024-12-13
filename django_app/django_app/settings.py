6275b0b603bfa440ad7457f55a301b747676e971
kmjak <tymkoron1221@icloud.com>
2024-12-13 14:18:09 +0900

    ホットリロードの削除とmiddlewareを使えるように変更した

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_browser_reload",
     'employee',
 ]
 
@@ -49,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'employee.acl_middleware.AclMiddleware', 
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
